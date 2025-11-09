575a745a561ef923e1169e841a00b172afb7b8d0
Kaitlin Yandik <kmy1691@g.rit.edu>
2025-11-09 11:04:51 -0500

    add more sudokus, prep for depth checking

---
+++ code/graph_performance.py
@@ -0,0 +1,15 @@
+import csv
+from backtrack import CSP_Sudoku
+from sudoku import SudokuState
+from ac3 import AC3Solver
+
+SUDOKU_FILES = [
+    "data/sudoku1.csv",
+    "data/sudoku2.csv",
+    "data/sudoku3.csv",
+    "data/sudoku4.csv",
+    "data/sudoku5.csv",
+    "data/sudoku6.csv",
+    "data/sudoku7.csv",
+    "data/sudoku8.csv"
+]
